c6ea5a01c3772f706a8e17e53afd70d76ae7c81a
ArchilMarukashvili <achiko2002@hotmail.com>
2025-11-12 20:29:03 +0000

    Added difficulty levels

--- main.c
+++ main.c
@@ -1,13 +1,33 @@
-#include <stdio.h> //Basic input-output library, needed for printf() and scanf()
-#include <stdlib.h> //General library needed for rand() and srand()
-#include <time.h> //Library needed to seed the random number generator
+#include <stdio.h>   // Basic input-output library, needed for printf() and scanf()
+#include <stdlib.h>  // General library needed for rand() and srand()
+#include <time.h>    // Library needed to seed the random number generator
 
 int main() {
-    int number, guess, attempts = 0; //Declaring 3 variables, with attempts being the only one initialized to 0
-    srand(time(0));
-    number = rand() % 100 + 1;
+    int number, guess, attempts = 0; // Declaring variables, initializing attempts to 0
+    int level, max; // Variables for difficulty selection
 
-    printf("Guess the number between 1 and 100\n");
+    // Ask for difficulty level
+    printf("Select difficulty: 1 = Easy (1-50), 2 = Medium (1-100), 3 = Hard (1-500): ");
+    scanf("%d", &level); //scanf pauses program, waits for user input, %d tells program to make sure input is a base 10 value,
+    // and then assigns whatever number is inputted to the memory address of level
+
+    // Determine number range based on chosen difficulty
+    switch (level) {
+        case 1: max = 50;  break;
+        case 2: max = 100; break;
+        case 3: max = 500; break;
+        default:
+            printf("Invalid choice. Defaulting to Medium (1-100).\n"); //If invalid input entered, automatically set difficulty to medium
+
+            max = 100;
+            break;
+    }
+
+    srand(time(0));           // Seed the random number generator
+    number = rand() % max + 1; // Generate random number within chosen range, we add +1 because range begins from 0
+
+    printf("Guess the number between 1 and %d\n", max);
+
 
     do {
         printf("Enter your guess: ");
@@ -21,13 +41,12 @@ int main() {
         else
             printf("Correct! You guessed it in %d attempts.\n", attempts);
 
-    } while (guess != number); //repeats loop until correct guess
+    } while (guess != number); // Repeat until the correct guess
 
     return 0;
 }
 
-//In order to run the code, first compile it with:
+// You'll need to compile the code first using a C compiler like GCC, everytime prorgram is edited:
 // gcc main.c -o main.exe
-
-//Then execute the compiled program with:
+// Then to simply run it you run:
 // .\main.exe
